179440410746da930197dfd34d8bdd35157883f7
clifton <cliftonavil@gmail.com>
2019-09-03 21:30:01 +0530

    final

--- main.go
+++ main.go
@@ -1,7 +1,92 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
+
+type Product struct {
+	gorm.Model
+	Code  string
+	Price string
+	Units string
+}
 
 func main() {
-	fmt.Println("hi!!!")
+	db, err := gorm.Open("sqlite3", "test.db")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer db.Close()
+	router := mux.NewRouter()
+
+	// router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	var template = template.Must(template.ParseFiles("index.html"))
+	// 	Activity := []Product{}
+	// 	db.Find(&Activity)
+	// 	template.ExecuteTemplate(w, "index.html", Activity)
+	// })
+
+	// router.HandleFunc("/add/", func(w http.ResponseWriter, r *http.Request) {
+	// 	http.ServeFile(w, r, "add.html")
+	// })
+
+	// router.HandleFunc("/insert/", func(w http.ResponseWriter, r *http.Request) {
+	// 	var template = template.Must(template.ParseFiles("index.html"))
+	// 	if r.Method == "POST" {
+	// 		name := r.FormValue("name")
+	// 		price := r.FormValue("price")
+	// 		unit := r.FormValue("units")
+	// 		fmt.Println(name, price, unit)
+	// 		// Create
+	// 		db.Create(&Product{Code: name, Price: price, Units: unit})
+	// 		Activity := []Product{}
+	// 		db.Find(&Activity)
+	// 		template.ExecuteTemplate(w, "index.html", Activity)
+	// 	} else {
+	// 		http.ServeFile(w, r, "add.html")
+	// 	}
+	// })
+
+	// router.HandleFunc("/delete/{id}/", func(w http.ResponseWriter, r *http.Request) {
+	// 	var template = template.Must(template.ParseFiles("index.html"))
+	// 	vars := mux.Vars(r)
+	// 	id := vars["id"]
+	// 	var product Product
+	// 	db.Where("id = ?", id).Delete(&product)
+
+	// 	Activity := []Product{}
+	// 	db.Find(&Activity)
+	// 	template.ExecuteTemplate(w, "index.html", Activity)
+	// })
+
+	// router.HandleFunc("/edit/{id}/", func(w http.ResponseWriter, r *http.Request) {
+	// 	var template = template.Must(template.ParseFiles("edit.html"))
+	// 	vars := mux.Vars(r)
+	// 	id := vars["id"]
+	// 	Activity := []Product{}
+	// 	db.First(&Activity, id)
+	// 	template.ExecuteTemplate(w, "edit.html", Activity)
+	// })
+	// router.HandleFunc("/update/", func(w http.ResponseWriter, r *http.Request) {
+	// 	var template = template.Must(template.ParseFiles("index.html"))
+	// 	id := r.FormValue("id")
+	// 	name := r.FormValue("name")
+	// 	price := r.FormValue("price")
+	// 	units := r.FormValue("units")
+	// 	var product Product
+	// 	db.First(&product, id).Update(map[string]interface{}{"Price": price, "Code": name, "Units": units})
+	// 	Activity := []Product{}
+	// 	db.Find(&Activity)
+	// 	template.ExecuteTemplate(w, "index.html", Activity)
+	// })
+
+	servererror := http.ListenAndServe(":8080", router)
+	if servererror != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
 }
