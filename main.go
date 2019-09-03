c8694875c92de14a1ec85d82736c1c0caec1a841
clifton <cliftonavil@gmail.com>
2019-09-03 22:22:57 +0530

    final

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,28 +11,68 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
-type Product struct {
+type User struct {
 	gorm.Model
-	Code  string
-	Price string
-	Units string
+	Name  string
+	Email string
+}
+
+var Database *gorm.DB
+
+func init() {
+	var err error
+
+	Database, err = gorm.Open("sqlite3", "test.db")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// set this to 'true' to see sql logs
+	Database.LogMode(true)
+
+	fmt.Println("Database connection successful.")
+	// Database.Debug().CreateTable(&User{})
+	// var user []User = []User{
+	// 	User{Name: "Clifton", Email: "cliftonavil@gmail.com"},
+	// 	User{Name: "Austin", Email: "austindsouza41@gmail.com"},
+	// }
+
+	// for _, user := range user {
+	// 	Database.Debug().Create(&user)
+	// }
 }
 
 func main() {
-	db, err := gorm.Open("sqlite3", "test.db")
-	if err != nil {
-		panic("failed to connect database")
-	}
-	defer db.Close()
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", homeLink).Methods("GET")
 
-	servererror := http.ListenAndServe(":8080", router)
-	if servererror != nil {
-		log.Fatal("ListenAndServe:", err)
-	}
+	router.HandleFunc("/users", allUsers).Methods("GET")
+	router.HandleFunc("/user/{id}", deleteUser).Methods("DELETE")
+	router.HandleFunc("/user/{name}/{email}", updateUser).Methods("PUT")
+	router.HandleFunc("/user/{name}/{email}", newUser).Methods("POST")
+
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
-func homeLink(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome home!")
+func allUsers(w http.ResponseWriter, r *http.Request) {
+	var users []User
+	Database.Find(&users)
+	fmt.Println("{}", users)
+
+	json.NewEncoder(w).Encode(users)
+}
+
+func newUser(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "New User Endpoint Hit")
+}
+
+func deleteUser(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	fmt.Println("id", id)
+	var users User
+	Database.Where("id = ?", id).Delete(&users)
+}
+
+func updateUser(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Update User Endpoint Hit")
 }
